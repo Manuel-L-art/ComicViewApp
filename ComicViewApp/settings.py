b48467a428639f47de29767b1984478477edc4f8
Manuel Lopez <lopez1manuel@gmail.com>
2021-05-25 21:56:53 -0700

    added gitignore

--- ComicViewApp/settings.py
+++ ComicViewApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+from secrets import *
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&pmmsl&f1qz_%le5qna#uxhb6=j867e#b+t6hww=fhip4(=ltw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
